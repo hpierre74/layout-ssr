d272af58aeb6090376c33bd990a84b5d5b99535f
Pierre Huyghe <pierrehuyghe.pro@gmail.com>
2019-11-24 18:07:38 +0100

    feat(config/content): add fetch content middleware and config init

---
+++ src/modules/config/config.reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  pages: {},
+  modules: {},
+  style: {},
+};
+
+export default function reducer(state = initialState) {
+  return state;
+}
--- src/utils/encrypt.utils.js
+++ src/utils/encrypt.utils.js
@@ -1,6 +1,6 @@
 import CryptoJS from 'crypto-js';
 
-export const encryptString = string => CryptoJS.AES.encrypt(string, process.env.REACT_APP_MESSAGE_KEY).toString();
+export const encryptString = string => CryptoJS.AES.encrypt(string, process.env.RAZZLE_SECRET_MESSAGE_KEY).toString();
 export const decryptHash = hash => {
-  return CryptoJS.AES.decrypt(hash, process.env.REACT_APP_MESSAGE_KEY).toString(CryptoJS.enc.Utf8);
+  return CryptoJS.AES.decrypt(hash, process.env.RAZZLE_SECRET_MESSAGE_KEY).toString(CryptoJS.enc.Utf8);
 };
